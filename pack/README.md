03399010c619867bf52ea5714be76150ef637748
Chris Marget <cmarget@juniper.net>
2025-07-24 13:42:50 -0400

    initial commit

--- pack/README.md
+++ pack/README.md
@@ -1,5 +1,3 @@
-# example
+# STIG-storm-control
 
-This pack is primarily a template intended to help pack publishers put together their own packs and pack release workflows.
-
-It a simple example which creates a single _configlet_ in the Apstra global catalog.
+Enable storm control on L2 ports at the edge of the fabric.
--- pack/tofu/configlet.tf
+++ pack/tofu/configlet.tf
@@ -2,20 +2,44 @@
 #  All rights reserved.
 #  SPDX-License-Identifier: MIT
 
-resource "apstra_configlet" "example" {
+resource "apstra_datacenter_configlet" "example" {
+  blueprint_id = var.blueprint_id
+  condition    = "role in [\"leaf\", \"access\"]"
   name = var.name
   generators = [
     {
       config_style  = "junos"
       section       = "top_level_hierarchical"
       template_text = <<-EOT
-        ether-options {
-          802.3ad {
-            lacp {
-              force-up;
+        forwarding-options {
+            storm-control-profiles default {
+                all;
+            }
+          }
+        {% for int, int_info in interface.items() %}
+        {% if ((int_info.role == "l2edge") and (int_info.lag_mode is none )) %}
+        interfaces {
+          {{ int_info.intfName }} {
+            unit 0 {
+              family ethernet-switching {
+                storm-control default;
+              }
             }
           }
         }
+        {% endif %}
+        {% if ((int_info.role == "l2edge") and ('ae' in int_info.intfName) ) %}
+        interfaces {
+          {{ int_info.intfName }} {
+            unit 0 {
+              family ethernet-switching {
+                storm-control default;
+              }
+            }
+          }
+        }
+        {% endif %}
+        {% endfor %}
       EOT
     },
   ]
